3bd729c2061e98a8907745ef49808a6173376119
oticstps <oticsjetson@gmail.com>
2026-03-05 08:23:32 +0700

    Create tb_hikitori.sql

---
+++ Server_Raspi_Meja_Wanda/169.254.33.135/tb_hikitori.sql
@@ -0,0 +1,125 @@
+CREATE TABLE hikitori_data (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    name_hikitori VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    actual_pouling VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    loading_time VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    status VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    cycle_normal VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    andon VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE hikitori_a (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    name_hikitori VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    actual_pouling VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    loading_time VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    status VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    cycle_normal VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    andon VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE hikitori_b (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    name_hikitori VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    actual_pouling VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    loading_time VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    status VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    cycle_normal VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    andon VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE hikitori_c (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    name_hikitori VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    actual_pouling VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    loading_time VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    status VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    cycle_normal VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    andon VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE hikitori_d (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    name_hikitori VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    actual_pouling VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    loading_time VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    status VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    cycle_normal VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    andon VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE hikitori_e (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    name_hikitori VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    actual_pouling VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    loading_time VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    status VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    cycle_normal VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    andon VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE hikitori_f (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    name_hikitori VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    actual_pouling VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    loading_time VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    status VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    cycle_normal VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    andon VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE hikitori_g (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    name_hikitori VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    actual_pouling VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    loading_time VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    status VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    cycle_normal VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    andon VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE hikitori_h (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    name_hikitori VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    actual_pouling VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    loading_time VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    status VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    cycle_normal VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    andon VARCHAR(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB 
+DEFAULT CHARSET=utf8mb4 
+COLLATE=utf8mb4_general_ci;
