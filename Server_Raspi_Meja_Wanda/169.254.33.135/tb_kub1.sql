12a832b933c557356e22e7bc940a7274df4fe7ff
oticstps <oticsjetson@gmail.com>
2026-03-05 10:16:49 +0700

    Update tb_kub1.sql

--- Server_Raspi_Meja_Wanda/169.254.33.135/tb_kub1.sql
+++ Server_Raspi_Meja_Wanda/169.254.33.135/tb_kub1.sql
@@ -11,12 +11,10 @@ CREATE TABLE tb_kub_active_power (
     active_power_c DECIMAL(12,3) NULL COMMENT 'Daya Aktif Fasa C (kW)',
 
     INDEX idx_datetime (date_time),
-    INDEX idx_panel (panel)
+    INDEX idx_panel (panel)a
 ) ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_uca1400_ai_ci;
-
-
+COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE tb_kub_apparent_power (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -26,13 +24,11 @@ CREATE TABLE tb_kub_apparent_power (
     apparent_power_a DECIMAL(10,3) NULL DEFAULT NULL,
     apparent_power_b DECIMAL(10,3) NULL DEFAULT NULL,
     apparent_power_c DECIMAL(10,3) NULL DEFAULT NULL,
-
     INDEX idx_datetime (date_time),
     INDEX idx_panel (panel)
 ) ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_uca1400_ai_ci;
-
+COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE tb_kub_current (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -43,28 +39,22 @@ CREATE TABLE tb_kub_current (
     current_b DECIMAL(10,3) NULL DEFAULT NULL,
     current_c DECIMAL(10,3) NULL DEFAULT NULL,
     current_n DECIMAL(10,3) NULL DEFAULT NULL,
-
     INDEX idx_datetime (date_time),
     INDEX idx_panel (panel)
 ) ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_uca1400_ai_ci;
-
-
+COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE tb_kub_frequency (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     date_time DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
     panel VARCHAR(50) NULL DEFAULT NULL,
     data_freq DECIMAL(10,3) NULL DEFAULT NULL,
-
     INDEX idx_datetime (date_time),
     INDEX idx_panel (panel)
 ) ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_uca1400_ai_ci;
-
-
+COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE tb_kub_power_factor (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -74,13 +64,11 @@ CREATE TABLE tb_kub_power_factor (
     power_factor_a DECIMAL(5,3) NULL DEFAULT NULL,
     power_factor_b DECIMAL(5,3) NULL DEFAULT NULL,
     power_factor_c DECIMAL(5,3) NULL DEFAULT NULL,
-
     INDEX idx_datetime (date_time),
     INDEX idx_panel (panel)
 ) ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_uca1400_ai_ci;
-
+COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE tb_kub_reactive_power (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -90,12 +78,11 @@ CREATE TABLE tb_kub_reactive_power (
     reactive_power_a DECIMAL(10,3) NULL DEFAULT NULL,
     reactive_power_b DECIMAL(10,3) NULL DEFAULT NULL,
     reactive_power_c DECIMAL(10,3) NULL DEFAULT NULL,
-
     INDEX idx_datetime (date_time),
     INDEX idx_panel (panel)
 ) ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_uca1400_ai_ci;
+COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE tb_kub_voltage_ll (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -105,13 +92,11 @@ CREATE TABLE tb_kub_voltage_ll (
     voltage_ab DECIMAL(10,3) NULL DEFAULT NULL,
     voltage_bc DECIMAL(10,3) NULL DEFAULT NULL,
     voltage_ca DECIMAL(10,3) NULL DEFAULT NULL,
-
     INDEX idx_datetime (date_time),
     INDEX idx_panel (panel)
 ) ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_uca1400_ai_ci;
-
+COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE tb_kub_voltage_ln (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -121,15 +106,8 @@ CREATE TABLE tb_kub_voltage_ln (
     voltage_an DECIMAL(10,3) NULL DEFAULT NULL,
     voltage_bn DECIMAL(10,3) NULL DEFAULT NULL,
     voltage_cn DECIMAL(10,3) NULL DEFAULT NULL,
-
     INDEX idx_datetime (date_time),
     INDEX idx_panel (panel)
 ) ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_uca1400_ai_ci;
-
-
-
-
-
-
+COLLATE=utf8mb4_unicode_ci;
